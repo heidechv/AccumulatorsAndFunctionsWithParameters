29a1bcf90ed364c1485d5971f3cccc96bff02530
heidechv <heidechv@rose-hulman.edu>
2017-11-30 13:00:59 -0500

    m7 finished

--- src/m7_why_parameters_are_powerful.py
+++ src/m7_why_parameters_are_powerful.py
@@ -64,7 +64,7 @@ def draw_circles():
     window.close_on_mouse_click()
 
 # ----------------------------------------------------------------------
-# TODO: 2.
+# DONE: 2.
 #   First, RUN this program.  You will see that draw_circles draws
 #   concentric circles whose radii vary by 10.
 #
